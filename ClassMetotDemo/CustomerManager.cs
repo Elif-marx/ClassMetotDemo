1aab5fc17e2c14cc056b1386bad067a8d71ae7db
Elif-marx <elifnazkaya_01@hotmail.com>
2021-01-26 15:25:29 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("customer attached");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("customer deleted");
+        }
+
+        public void List(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("customer name " + customer.CustomerName);
+                Console.WriteLine("customer surname " + customer.CustomerSurname);
+                Console.WriteLine("customer ıd number " + customer.CustomerId);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 222;
+            customer1.CustomerName = "Elifnaz";
+            customer1.CustomerSurname = "Kaya";
+
+            Customer customer2 = new Customer();
+            customer2.CustomerId = 333;
+            customer2.CustomerName = "Aleyna";
+            customer2.CustomerSurname = "Köksal";
+
+            Customer customer3 = new Customer();
+            customer3.CustomerId = 444;
+            customer3.CustomerName = "Elif";
+            customer3.CustomerSurname = "Gıramoğlu";
+
+
+
+            CustomerManager custom = new CustomerManager();
+            custom.Add(customer1);
+            custom.Delete(customer1);
+            custom.Add(customer2);
+            custom.Delete(customer2);
+            custom.Add(customer3);
+            custom.Delete(customer3);
+            Console.WriteLine("---------------------");
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+            custom.List(customers);
+
+
+        }
+    }
+}
